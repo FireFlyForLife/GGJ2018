9f355f4edf0553cbfcb4550efb6c65c720aa0c64
FireFlyForLife <maiko.s.99@gmail.com>
2018-01-26 20:23:59 +0100

    initial commit

---
+++ Assets/Scripts/ConstantRotatoin.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstantRotatoin : MonoBehaviour
+{
+    public float RotationSpeed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    var euler = transform.rotation.eulerAngles;
+	    euler.y += RotationSpeed * Time.deltaTime;
+	    transform.rotation = Quaternion.Euler(euler);
+	}
+}
